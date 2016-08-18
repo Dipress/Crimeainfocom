b84b5aa6c85e64a3b7da2028d3947c4457a11822
Dipress <budnikovdo@gmail.com>
2016-08-18 17:03:34 +0300

    config deploy unicorn + capistrano

--- Gemfile
+++ Gemfile
@@ -58,6 +58,10 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+group :production do
+  gem 'unicorn'
+end
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
@@ -81,6 +85,8 @@ group :development do
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
   gem 'annotate'
+  gem 'rvm-capistrano'
+  gem 'capistrano'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
---
+++ config/deploy.rb
@@ -0,0 +1,60 @@
+set :rvm_ruby_string, "rvm use 2.3.0@crimeainfocom-rails5"
+require "rvm/capistrano"
+require "bundler/capistrano"
+
+set :rvm_type, :system
+set :rvm_path, "/usr/local/rvm"
+
+set :application, "crimeainfocom"
+set :repository,  "git@github.com:Dipress/Crimeainfocom.git"
+
+set :scm, :git
+set :user, "root"
+set :use_sudo, false
+
+set :branch, "master"
+set :deploy_via, :remote_cache
+set :keep_releases, 3
+set :deploy_to, "/var/www/apps/#{application}"
+set :rails_env, "production"
+set :domain, "194.54.152.50"
+set :scm_command, "/usr/bin/git"
+set :scm_verbose, true
+set :normalize_asset_timestamps, false
+
+set :unicorn_conf, "#{deploy_to}/current/config/unicorn.rb"
+set :unicorn_pid, "#{deploy_to}/shared/pids/unicorn.pid"
+
+role :web, domain
+role :app, domain
+role :db,  domain, primary: true
+
+ssh_options[:forward_agent] = true
+ssh_options[:keys] = [File.join(ENV["HOME"], ".ssh", "id_rsa")]
+
+namespace :deploy do
+  desc 'Unicorn restart'
+  task :restart do
+    run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -USR2 `cat #{unicorn_pid}`; else cd #{latest_release} && bundle exec unicorn_rails -c #{unicorn_conf} -E #{rails_env} -D; fi"
+  end
+
+  desc 'Unicorn start'
+  task :start do
+    run "cd #{latest_release} && bundle exec unicorn_rails -c #{unicorn_conf} -E #{rails_env} -D"
+  end
+
+  desc 'Unicorn stop'
+  task :stop do
+    run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -QUIT `cat #{unicorn_pid}`; fi"
+  end
+
+  desc 'Create symlinks on files'
+  task :symlinks do
+    run "ln -s /var/www/crimeainfocom/database.yml #{current_release}/config/database.yml"
+    run "ln -s /var/www/crimeainfocom/secrets.yml #{current_release}/config/secrets.yml"
+    run "ls -s /var/www/crimeainfocom/uploads #{current_release}/public/uploads" 
+  end
+end
+
+before "deploy:assets:precompile", "deploy:symlinks"
+after "deploy", "deploy:cleanup", "deploy:restart"
